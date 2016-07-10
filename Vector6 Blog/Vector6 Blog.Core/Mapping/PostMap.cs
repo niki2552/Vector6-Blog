59b8d8fea1814cf452f5de94a60b0363d27c1fde
niki2552 <pe_ni_ne_vd@abv.bg>
2016-07-10 16:42:58 +0300

    trabva da si dovyrsha klasa postmap

---
+++ Vector6 Blog/Vector6 Blog.Core/Mapping/PostMap.cs
@@ -0,0 +1,19 @@
+﻿using FluentNHibernate.Mapping;
+using Vector6_Blog.Core.Objects;
+
+namespace Vector6_Blog.Core.Mapping
+{
+    public class PostMap:ClassMap<Post>
+    {
+        public PostMap ()
+        {
+            Id(x => x.Id);
+
+            Map(x => x.Title)
+                .Length(500)
+                .Not.Nullable();
+
+
+        }
+    }
+}
