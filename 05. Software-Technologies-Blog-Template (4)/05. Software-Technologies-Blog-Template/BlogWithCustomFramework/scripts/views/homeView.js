ca07423c0dfdaad3412f35af96f606a089e53194
fiher <fihercho@gmail.com>
2016-09-01 03:41:49 +0300

    AlmostReady

--- 05. Software-Technologies-Blog-Template (4)/05. Software-Technologies-Blog-Template/BlogWithCustomFramework/scripts/views/homeView.js
+++ 05. Software-Technologies-Blog-Template (4)/05. Software-Technologies-Blog-Template/BlogWithCustomFramework/scripts/views/homeView.js
@@ -6,33 +6,33 @@ class HomeView{
         this._mainContentSelector = mainContentSelector;
         this._wrapperSelector = wrapperSelector;
     }
-    showGuestPage(mainData,sidebarData) {
+    showGuestPage(mainData,) {
         let _that = this;
-       $.get('templates/welcome-guest.html', function(template){
+       $.get('templates/home-guest.html', function(template){
            let renderedTemplate = Mustache.render(template,null);
          console.log('maina');
 
            $(_that._wrapperSelector).html(renderedTemplate);
 
-           $.get('templates/posts.html',function(template){
-              let blogPosts = {
-                  blogPosts:mainData
-              };
-               
-
-               let renderedPosts = Mustache.render(template,blogPosts);
-               $('.articles').html(renderedPosts);
-           });
-           $.get('templates/recent-posts.html',function(template){
-               let recentPosts = {
-                   recentPosts:sidebarData
-               };
-
-
-               let renderedPosts = Mustache.render(template,recentPosts);
-               $('.recent-posts').html(renderedPosts);
-           });
-
+           //$.get('templates/posts.html',function(template){
+           //   let blogPosts = {
+           //       blogPosts:mainData
+           //   };
+           //    
+//
+           //    let renderedPosts = Mustache.render(template,blogPosts);
+           //    $('.articles').html(renderedPosts);
+           //});
+           //$.get('templates/recent-posts.html',function(template){
+           //    let recentPosts = {
+           //        recentPosts:sidebarData
+           //    };
+//
+//
+           //    let renderedPosts = Mustache.render(template,recentPosts);
+           //    $('.recent-posts').html(renderedPosts);
+           //});
+//
 
        });
     }
